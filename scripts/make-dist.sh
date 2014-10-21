f5de5891c257524895fc787f6cc53b7962e66a1a
Braydon Fuller <braydon@bitpay.com>
2014-10-20 20:18:03 -0400

    use projects locally installed browserify

--- scripts/make-dist.sh
+++ scripts/make-dist.sh
@@ -4,7 +4,7 @@ node browser/build -s lib/Key,lib/SINKey,lib/SIN,util/util
 cd ../../
 cp node_modules/bitcore/browser/bundle.js dist/bitcore.bundle.js
 echo "Building browser bundle for bitauth..."
-browserify lib/bitauth.js -s bitauth -x buffertools -x bitcore -o dist/bitauth.bundle.js
+node_modules/.bin/browserify lib/bitauth.js -s bitauth -x buffertools -x bitcore -o dist/bitauth.bundle.js
 echo "Minifying bitcore and bitauth..."
 node_modules/.bin/uglifyjs dist/bitcore.bundle.js dist/bitauth.bundle.js -o dist/bitauth.browser.min.js
 echo "Done!"
