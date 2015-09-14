2052c617828691f0aca9a52623cbb7c183d84725
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2015-09-14 09:42:45 -0600

    Closes https://github.com/bitpay/bitauth/issues/42

--- scripts/make-dist.sh
+++ scripts/make-dist.sh
@@ -1,5 +1,19 @@
-echo "Building browser bundle for bitauth..."
-node_modules/.bin/browserify lib/bitauth.js -s bitauth -o dist/bitauth.bundle.js
-echo "Minifying bitauth..."
-node_modules/.bin/uglifyjs dist/bitauth.bundle.js --compress --mangle -o dist/bitauth.browser.min.js
+#!/bin/bash
+
+# Unofficial strict mode
+set -euo pipefail
+IFS=$'\n\t'
+
+# Directory of this script
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+[ -d ${DIR}/../dist ] || mkdir ${DIR}/../dist
+
+(cd ${DIR}/.. ;
+ echo "Building browser bundle for bitauth..." ;
+ # Bundle puts full path names in its output, so pipe to sed to kill that noise
+ ./node_modules/.bin/browserify lib/bitauth.js -s bitauth | \
+	 sed -e "s|${HOME}/.*/\(.*\.js\)|\1|" > ./dist/bitauth.bundle.js ;
+ echo "Minifying bitauth..." ;
+ ./node_modules/.bin/uglifyjs ./dist/bitauth.bundle.js --compress --mangle -o ./dist/bitauth.browser.min.js)
 echo "Done!"
